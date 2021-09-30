b495478ae27f5eb73439994e715007e426a781c6
Yuliia Bondarenko <bondarenko047@gmail.com>
2021-09-30 12:50:44 +0300

    readme

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # goit-nodejs-hw-01
+
+# Получаем и выводим весь список контактов
+
+node index.js --action list
+https://monosnap.com/file/Wn4gOmcwUJ1UWSMEC2JKoV8vHHWzYZ
+
+# Получаем контакт по id
+
+node index.js --action get --id 5
+https://monosnap.com/file/uHGY8a0KnjCBzkRFnJLNiHbvWLtv9v
+
+# Добавялем контакт
+
+node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
+https://monosnap.com/file/6q0tefSZN13C5MIQlzaztDDHwWhTPx
+
+# Удаляем контакт
+
+node index.js --action remove --id=3
+https://monosnap.com/file/XpUHQc17ptElBI9FQrAjIU3eYBps3y
--- index.js
+++ index.js
@@ -1,43 +1,129 @@
 const contactsOperations = require('./contacts');
-// console.log('Heloooo')
 
-const id = 11;
-const delId = 11;
+// // ---===== Проверка работоспособности функций для работы с контактами =====---
+// const id = 11;
+// const delId = 11;
 
-const newData = {
-    "name": "Bond Yuliia",
-    "email": "bond_yuv@dictum.co.uk",
-    "phone": "(050) 111-2222"
-};
+// const newData = {
+//     "name": "Bond Yuliia",
+//     "email": "bond_yuv@dictum.co.uk",
+//     "phone": "(050) 111-2222"
+// };
 
-(async()=>{
-    try {
-        // const contacts = await contactsOperations.listContacts();
-        // console.log(contacts);
+// (async()=>{
+//     try {
+//         // const contacts = await contactsOperations.listContacts();
+//         // console.log(contacts);
 
-        // const oneContact = await contactsOperations.getContactById(id);
-        // if(!oneContact){
-        //     throw new Error(`Товар с id=${id} не найден`);
-        // }
-        // console.log(oneContact);
+//         // const oneContact = await contactsOperations.getContactById(id);
+//         // if(!oneContact){
+//         //     throw new Error(`Товар с id=${id} не найден`);
+//         // }
+//         // console.log(oneContact);
 
-        // const newContact = await contactsOperations.addContact(newData);
-        // console.log(newContact);
+//         // const newContact = await contactsOperations.addContact(newData);
+//         // console.log(newContact);
 
-        // const updateContact = await contactsOperations.getContactById(id, { ...oneContact, phone: "(050) 333-4444" });
-        // if(!updateContact){
-        //     throw new Error(`Товар с id=${id} не найден`);
-        // }
-        // console.log(updateContact);
+//         // const updateContact = await contactsOperations.getContactById(id, { ...oneContact, phone: "(050) 333-4444" });
+//         // if(!updateContact){
+//         //     throw new Error(`Товар с id=${id} не найден`);
+//         // }
+//         // console.log(updateContact);
 
-        // const result = await contactsOperations.removeContact(delId);
-        // if(!result){
-        //     throw new Error(`Контакт с id=${delId} не найден`);
-        // }
-        // console.log("Удаление прошло успешно");
-    } 
-    catch (error) {
-        console.log(error.message);
+//         // const result = await contactsOperations.removeContact(delId);
+//         // if(!result){
+//         //     throw new Error(`Контакт с id=${delId} не найден`);
+//         // }
+//         // console.log("Удаление прошло успешно");
+//     } 
+//     catch (error) {
+//         console.log(error.message);
+//     }
+// })();
+
+const argv = require('yargs').argv;
+
+// // ---===== YARGS =====---
+// async function invokeAction({ action, id, name, email, phone }) {
+//     switch (action) {
+//         case 'list':
+//             const contacts = await contactsOperations.listContacts();
+//             console.log(contacts);
+//         break;
+
+//         case 'get':
+//             const oneContact = await contactsOperations.getContactById(id);
+//             if (!oneContact){
+//                 throw new Error(`Товар с id=${id} не найден`);
+//             }
+//             console.log(oneContact);
+//         break;
+
+//         case 'add':
+//             await contactsOperations.addContact({name, email, phone})
+//             console.log("Контакт добавлен");
+//         break;
+
+//         case 'remove':
+//             const result = await contactsOperations.removeContact(id);
+//             if(!result){
+//                 throw new Error(`Контакт с id=${id} не найден`);
+//             }
+//             console.log("Удаление прошло успешно");
+//         break;
+
+//         default:
+//         console.warn('\x1B[31m неизвестный тип действия!');
+//     }
+// }
+
+// invokeAction(argv);
+
+const { program } = require('commander');
+// ---===== COMMANDER =====---
+
+program
+    .option('-a, --action <type>', 'choose action')
+    .option('-i, --id <type>', 'user id')
+    .option('-n, --name <type>', 'user name')
+    .option('-e, --email <type>', 'user email')
+    .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+const options = program.opts();
+// console.log(options);
+
+async function invokeAction({ action, id, name, email, phone }) {
+    switch (action) {
+        case 'list':
+            const contacts = await contactsOperations.listContacts();
+            console.table(contacts);
+        break;
+
+        case 'get':
+            const oneContact = await contactsOperations.getContactById(id);
+            if (!oneContact){
+                throw new Error(`Товар с id=${id} не найден`);
+            }
+            console.log(oneContact);
+        break;
+
+        case 'add':
+            await contactsOperations.addContact({ name, email, phone })
+            console.log("Контакт добавлен");
+        break;
+
+        case 'remove':
+            const result = await contactsOperations.removeContact(id);
+            if(!result){
+                throw new Error(`Контакт с id=${id} не найден`);
+            }
+            console.log("Удаление прошло успешно");
+        break;
+
+        default:
+        console.warn('\x1B[31m неизвестный тип действия!');
     }
-})();
+}
 
+invokeAction(argv);
